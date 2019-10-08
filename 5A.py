3363060745f63ac8e15fcf30249c924b5736c648
Marcus Örnås <maror974@student.liu.se>
2019-10-08 10:05:38 +0200

    klar med 5A

--- 5A.py
+++ 5A.py
@@ -1,39 +1,12 @@
 import cv2
 import numpy
 import math
-"""övning 501"""
-def imageturngrey():
-    filename = "image.jpg"
-
-    # Läser in bilden i gråskala till ett bildobjekt
-    image = cv2.imread(filename, 0)
-
-    # Visa bildobjektet
-    cv2.imshow('Flowers turned grey', image)
-
-    # Spara bildobjektet
-    cv2.imwrite('grey_flowers.jpg', image)
-
-    # Vänta på tangent
-    cv2.waitKey(0)
-
-    # Stäng ner alla OpenCV-fönster
-    cv2.destroyAllWindows()
-#Övning 501
-def blue():
-    
-    src = cv2.imread('image.jpg', cv2.IMREAD_UNCHANGED)
-    print(src)
-    # assign blue channel to zeros
-    src[:,:,0] = numpy.zeros([src.shape[0], src.shape[1]])
-    print(src)
-    #save image
-    cv2.imwrite('image2.jpg',src) 
-    cv2.imshow('image2.jpg', src)
-    cv2.waitKey(0)
+from cvlib import *
 
 #Uppgift 5A1
 def cvimg_to_list(img):
+    '''the function converts the opencv datastructure to a regular python list consisting of tuples'''
+
     tuplelist = []
     for i in range(len(img)):
         for r in range(len(img[0])):
@@ -42,26 +15,29 @@ def cvimg_to_list(img):
 
 #5A2
 def unsharp_mask(n):
+    '''creates a 2d list consiting of the gausik blur that can be used on images to make the image sharper'''
+
     nvalues = []
-    value = n
-    if value == n:
-        nvalues.append(0)
-        value = n-1
-    elif value != n and value > 0:
-        pos_value = value // 2 
-        neg_value = value // 2 + 1
-    
+    value = n-1
+    pos_value = value // 2 
+    neg_value = -(value // 2 )
+
     for column in range(n):
+        nvalues.append([])
         for row in range(n):
-            board[column][row] = 
-           
-
+            gaublur = blur(column-pos_value,row+neg_value)
+            nvalues[column].append(gaublur)
     
-
+    return nvalues           
 
 def blur(x, y):
+    '''creates a gausisk blur with the given math formula and special case if x and y is 0'''
+
+    if x == 0 and y == 0:
+        return 1.5
     a1 = 2*math.pi*(4.5**2)
     a2 = math.e**-(((x**2)+(y**2))/(2*(4.5**2)))
     G = (-1/a1)*a2
     return G
+
 
